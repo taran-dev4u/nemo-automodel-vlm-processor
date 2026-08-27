71bc4170208da999505590a43da37372afced169
Taran Mamidala <mamidalataran6333@gmail.com>
2026-08-27 04:56:19 -0400

    feat(errors): add VLMSerializationError hierarchy

---
+++ src/nemo_vlm_processor/exceptions.py
@@ -0,0 +1,4 @@
+"""Custom exceptions for VLM processor serialization."""
+class VLMSerializationError(Exception):
+    """Raised when VLM processor or tokenizer artifacts cannot be serialized."""
+    pass
