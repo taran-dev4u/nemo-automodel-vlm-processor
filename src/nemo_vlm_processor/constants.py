1be21fe28c94bd38a303893e8cd70a8e5b147aa2
Taran Mamidala <mamidalataran6333@gmail.com>
2026-08-27 04:56:18 -0400

    feat(constants): define supported VLM architecture mapping

---
+++ src/nemo_vlm_processor/constants.py
@@ -0,0 +1,5 @@
+"""Constants and supported VLM architecture families."""
+SUPPORTED_VLM_ARCHITECTURES = [
+    "gemma", "llama_vision", "qwen2_vl", "clip", "siglip", "blip-2"
+]
+DEFAULT_PROCESSOR_CONFIG_FILE = "preprocessor_config.json"
